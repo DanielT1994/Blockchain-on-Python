21222b37777a7f3d7438548bc4aa31771433c7a7
DanielT1994 <ahyperduck@gmail.com>
2022-02-15 12:02:41 +1100

    images

--- Starter_Code/pychain.py
+++ Starter_Code/pychain.py
@@ -196,7 +196,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        record = Record,
+        record = ("Sender:" ,sender, "Receiver:", receiver, "Amount:", amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
